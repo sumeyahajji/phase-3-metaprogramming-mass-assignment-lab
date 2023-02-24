a386f862f845b02120ddcb968dc8600bffe8635e
+ <+>
2023-02-24 16:39:17 +0300

    completed

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
 class Person
   # your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      # create a getter and setter by calling the attr_accessor method
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
+
 end
+
+bob_attributes = { name: "Bob", hair_color: "Brown" }
+bob = Person.new(bob_attributes)
+
+puts bob.name
+puts bob.hair_color
